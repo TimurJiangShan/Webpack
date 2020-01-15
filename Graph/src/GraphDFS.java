3668900f967e8e53e9580f38ed0d91e68e0787ed
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-01-15 21:25:04 +1100

    corner case

--- Graph/src/GraphDFS.java
+++ Graph/src/GraphDFS.java
@@ -8,6 +8,12 @@ public class GraphDFS {
     private ArrayList<Integer> pre = new ArrayList<>();
     private ArrayList<Integer> post = new ArrayList<>();
 
+    /*
+        二叉树的遍历：
+        前序遍历： 遍历发生在递归的前面。
+        中序遍历： 先去递归访问左子树， 再遍历当前节点， 再递归的访问右子树
+        后序遍历： 实际遍历的位置在两次递归的后面
+    */
     public GraphDFS(Graph G){
 
         this.G = G;
@@ -21,11 +27,11 @@ public class GraphDFS {
     private void dfs(int v){
 
         visited[v] = true;
-        pre.add(v);
+        pre.add(v);               // 深度优先先序遍历
         for(int w: G.adj(v))
             if(!visited[w])
                 dfs(w);
-        post.add(v);
+        post.add(v);              // 深度优先后序遍历
     }
 
     public Iterable<Integer> pre(){
